6426d27a2d2428a568ff3099e68c6a6c7394f9a9
Ken Kawashima <kawashimaken2015@gmail.com>
2020-02-04 19:45:40 +0900

    Create yes_or_no.py

---
+++ python/yes_or_no.py
@@ -0,0 +1,15 @@
+while True:
+  command = input('pybot...何をお探しですか、ほしいパーツを入力してください。>')
+  resoponse = ''
+  if 'cpu' in command:
+    resoponse = 'cpuはこちらです。'
+    print(resoponse)
+    command = input('pybot...こちらはどうですか？yesかnoでお答えください。 >')
+    if 'yes' in command:
+      resoponse = 'yesを選んでくれてありがとう'
+      print(resoponse)
+    if 'no' in command:
+      resoponse = 'noを選んでしまったんだね'
+      print(resoponse)
+  if 'さようなら' in command:
+    break
