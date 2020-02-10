c643c081b5f47a3b27443f2af948d0d9b996c5be
Ken Kawashima <kawashimaken2015@gmail.com>
2020-02-10 14:11:51 +0900

    update

--- pytorch/cifar10.py
+++ pytorch/cifar10.py
@@ -8,9 +8,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import torch.optim as optimizer
+# -----------------------------------------------------------------------------
+# MacOSで、下記のエラーに遭遇する人は、下の二行が必要
+# OMP: Error #15: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized.
+# OMP: Hint This means that multiple copies of the OpenMP runtime have been linked into the program. That is dangerous, since it can degrade performance or cause incorrect results. The best thing to do is to ensure that only a single OpenMP runtime is linked into the process, e.g. by avoiding static linking of the OpenMP runtime in any library. As an unsafe, unsupported, undocumented workaround you can set the environment variable KMP_DUPLICATE_LIB_OK=TRUE to allow the program to continue to execute, but that may cause crashes or silently produce incorrect results. For more information, please see http://www.intel.com/software/products/support/.
+
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
+# データフォーマット変更の設定
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
@@ -27,6 +33,7 @@ test_data_with_teacher_labels = torchvision.datasets.CIFAR10(
 test_data_loader = torch.utils.data.DataLoader(
     test_data_with_teacher_labels, batch_size=4, shuffle=False, num_workers=2)
 
+# クラス名（正解教師ラベル名）
 class_names = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse',
                'ship', 'truck')
 
@@ -51,38 +58,41 @@ class CNN(nn.Module):
         self.layer2 = nn.Linear(120, 84)
         # 全結合レイヤー3 出力層なので、最後は、ニューロン10個、これは分類したいクラスの数となります。
         self.layer3 = nn.Linear(84, 10)
+
     def forward(self, input_data):
         '''
         ネットワークの（順伝播）の定義（つなげる）
         '''
         # 1番目畳み込み層の活性化関数をReLUに指定、その上、プーリング
-        # 入力データはconv1に渡して、活性化関数ReLUを適用して、その上、プーリング層を加える
+        # 入力データはconv1に渡して、活性化関数ReLUを適用して、その上、プーリング層を加えます
         input_data = self.pool(F.relu(self.conv1(input_data)))
         # 2番目畳み込み層の活性化関数をReLUに指定、その上、プーリング
-        # 前の層からきたinput_dataをconv2に渡して、活性化関数ReLUを適用して、その上、プーリング層を加える
+        # 前の層からきたinput_dataをconv2に渡して、活性化関数ReLUを適用して、その上、プーリング層を加えます
         input_data = self.pool(F.relu(self.conv2(input_data)))
-        # 前の層からきたinput_dataをフォーマット変換する
+        # 前の層からきたinput_dataをフォーマット変換します
         # 自動的に変換する
         input_data = input_data.view(-1, 16 * 5 * 5)
-        # 前の層からきたinput_dataをlayer1に渡して、活性化関数ReLUを適用する
+        # 前の層からきたinput_dataをlayer1に渡して、活性化関数ReLUを適用します
         input_data = F.relu(self.layer1(input_data))
-        # 前の層からきたinput_dataをlayer2に渡して、活性化関数ReLUを適用する
+        # 前の層からきたinput_dataをlayer2に渡して、活性化関数ReLUを適用します
         input_data = F.relu(self.layer2(input_data))
-        # 前の層からきたinput_dataをlayer3(出力層)に渡す
+        # 前の層からきたinput_dataをlayer3(出力層)に渡します
         input_data = self.layer3(input_data)
         # 結果を返す
         return input_data
 
 
+# モデルのインスタンスを生成します
 model = CNN()
 
-# 損失関数は交差エントロピー誤差関数を使う
+# 損失関数は交差エントロピー誤差関数を使います
 criterion = nn.CrossEntropyLoss()
-# 最適化オプティマイザはSDGを使う、学習率lrは0.001、momentumを0.9に設定する
+# 最適化オプティマイザはSDGを使う、学習率lrは0.001、momentum(慣性項)を0.9に設定します
+# momentumが、ここではオプションですが、転がるボールが地面の摩擦抵抗で徐々に減速していくイメージです
 optimizer = optimizer.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 # 最大学習回数
-MAX_EPOCH = 3
+MAX_EPOCH = 1
 
 #
 for epoch in range(MAX_EPOCH):
@@ -92,7 +102,7 @@ for epoch in range(MAX_EPOCH):
         # dataから学習対象データと教師ラベルデータを取り出します
         train_data, teacher_labels = data
 
-        # 計算された勾配情報を削除します
+        # 計算された勾配情報を削除（リセット、クリア）します
         optimizer.zero_grad()
 
         # モデルでの予測を計算します
@@ -100,9 +110,10 @@ for epoch in range(MAX_EPOCH):
 
         # lossとwによる微分計算します
         loss = criterion(outputs, teacher_labels)
+        # 勾配を計算します
         loss.backward()
-
-        # 勾配を更新します
+        #
+        # 最適化のステップを一回実行します（パラメーターを更新します、たくさんのoptimizerの共通の処理）
         optimizer.step()
 
         # 誤差を累計します
@@ -116,12 +127,35 @@ for epoch in range(MAX_EPOCH):
 
 print('学習完了')
 
+# iter関数を使って、test_data_loaderをiteratorオブジェクトに変換します
 data_iterator = iter(test_data_loader)
+
+# iteratorのnext()関数を使って、一つずつ検証データを出して、アンプっくして、それぞれのの変数（imageとlables）に格納します
 images, labels = data_iterator.next()
 
+# imagesを学習済みモデルに入力して、その推論結果をoutputsに入れます
 outputs = model(images)
-_, predicted = torch.max(outputs, 1)
+print('outputs', outputs)
+# 結果：
+# tensor([[-0.5670, -0.5408, -0.1798,  1.7548, -1.1307,  1.7946, -0.0550, -0.4681,
+#           0.2456, -0.8611],
+#         [ 5.9728,  8.5670, -0.7799, -4.4108, -3.3220, -5.0610, -4.5656, -2.8008,
+#           6.8845,  1.8749],
+#         [ 1.8402,  2.5417, -0.7070, -1.0284, -1.3782, -1.8616, -2.0512, -1.3936,
+#           3.4731,  1.0065],
+#         [ 3.5036,  1.7275,  0.5213, -2.0472, -0.0412, -3.2352, -2.1648, -2.7098,
+#           4.5848,  0.2555]], grad_fn=<AddmmBackward>)
+# iteratorから出したデータはバッチになっており
+# ouputsも４枚の写真の推論結果配列が入っています
 
+# 一つずつ、推論結果配列の最大値（最も確信しているラベル）取り出します
+_, predicted = torch.max(outputs, 1)
+print('_', _)
+# 結果：それぞれの最大値そのものが入っています
+# tensor([1.6123, 5.6203, 3.0886, 3.8317], grad_fn=<MaxBackward0>)
+print('predicted', predicted)
+# 結果：「最大値は何番目なのか」が入っています
+# tensor([3, 9, 1, 0])
 print('予測: ', ' '.join('%5s' % class_names[predicted[j]] for j in range(4)))
 
 count_when_correct = 0
--- pytorch/cifar100.py
+++ pytorch/cifar100.py
@@ -8,9 +8,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import torch.optim as optimizer
+# -----------------------------------------------------------------------------
+# MacOSで、下記のエラーに遭遇する人は、下の二行が必要
+# OMP: Error #15: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized.
+# OMP: Hint This means that multiple copies of the OpenMP runtime have been linked into the program. That is dangerous, since it can degrade performance or cause incorrect results. The best thing to do is to ensure that only a single OpenMP runtime is linked into the process, e.g. by avoiding static linking of the OpenMP runtime in any library. As an unsafe, unsupported, undocumented workaround you can set the environment variable KMP_DUPLICATE_LIB_OK=TRUE to allow the program to continue to execute, but that may cause crashes or silently produce incorrect results. For more information, please see http://www.intel.com/software/products/support/.
+
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
+#
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
